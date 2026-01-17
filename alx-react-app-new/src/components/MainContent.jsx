68073208184d111f21bd760b404551806f5abab4
Reagile <moatshereagile@gmail.com>
2026-01-17 12:50:00 +0200

    Add alx-react-app-new project with Task 0 completed

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <div style={{ padding: '20px', backgroundColor: '#f0f0f0', color: '#333' }}>
+      <h2>Welcome to My Favorite Cities</h2>
+      <p>Explore some of the most beautiful cities in the world!</p>
+      {/* Add more content as needed */}
+    </div>
+  );
+}
+
+export default MainContent;
