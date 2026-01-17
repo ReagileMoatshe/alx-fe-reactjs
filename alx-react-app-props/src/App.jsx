55ba95ae71c5804c8890685ec41698356e0d0d49
Reagile <moatshereagile@gmail.com>
2026-01-17 14:51:32 +0200

    Implement Task 2 exactly as per instructions

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,18 +1,12 @@
-import { UserContext } from './UserContext';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
+import UserContext from './UserContext';
+import ProfilePage from './components/ProfilePage';
 
 function App() {
-  const userData = { name: "Alice", age: "25", bio: "Loves hiking and photography" };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
-      <Header />
-      <MainContent />
-      <Footer />
-      <UserProfile />
+      <ProfilePage />
     </UserContext.Provider>
   );
 }
--- alx-react-app-props/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer>
-            <p>(c) 2023 City Lovers</p>
-        </footer>
-    );
-}
-
-export default Footer;
