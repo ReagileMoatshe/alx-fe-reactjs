890f57cca9a804e4f2081ceb25e7d253d4c762d2
REAGILE MOATSHE <moatshereagile@gmail.com>
2026-01-17 18:25:36 +0200

    Update UserProfile.jsx

--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -1,16 +1,15 @@
 import { useContext } from 'react';
-import UserContext from '../UserContext';
+import { UserContext } from './UserContext';
 
-const UserProfile = () => {
-  const userData = useContext(UserContext);
+function UserProfile() {
+  const { userData } = useContext(UserContext);
 
   return (
     <div>
-      <h2>{userData.name}</h2>
-      <p>Age: {userData.age}</p>
-      <p>Bio: {userData.bio}</p>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
